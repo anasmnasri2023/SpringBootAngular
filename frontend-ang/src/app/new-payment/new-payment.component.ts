2af53a609c4a75c53b80169638cf9f149add1fc0
anasmnasri2023 <anes.mnasri@esprit.tn>
2025-03-10 03:52:12 +0100

    🚀 Initial commit: Spring Boot + Angular project setup

--- frontend-ang/src/app/new-payment/new-payment.component.ts
+++ frontend-ang/src/app/new-payment/new-payment.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {ActivatedRoute} from '@angular/router';
+import {paymentType} from '../model/students.model';
 
 @Component({
   selector: 'app-new-payment',
@@ -11,9 +12,16 @@ import {ActivatedRoute} from '@angular/router';
 export class NewPaymentComponent implements OnInit{
   paymentFormGroup!: FormGroup;
   studentCode!:string;
+  paymentTypes :string[]=[];
   constructor(private fb:FormBuilder,private activatedRoute: ActivatedRoute) {
   }
   ngOnInit() {
+    for (let element in paymentType) {
+      let value = paymentType[element]
+      if (typeof value == 'string') {
+        this.paymentTypes.push(value);
+      }
+    }
     this.studentCode=this.activatedRoute.snapshot.params['studentCode']
     this.paymentFormGroup= this.fb.group({
       date : this.fb.control(''),
