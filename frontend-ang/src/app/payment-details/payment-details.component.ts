1ff677e9d433facaad33b8120e77c8ea7e2184fb
anasmnasri2023 <anes.mnasri@esprit.tn>
2025-03-11 16:10:12 +0100

    ��FullStack

--- frontend-ang/src/app/payment-details/payment-details.component.ts
+++ frontend-ang/src/app/payment-details/payment-details.component.ts
@@ -10,13 +10,26 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class PaymentDetailsComponent implements OnInit{
   paymentId:number;
+  pdfFileUrl:any;
   constructor(private studentsService : StudentsService , private route:ActivatedRoute) {
   }
   ngOnInit() {
     this.paymentId=this.route.snapshot.params['id']
-    this.studentsService.getPaymenDetails(this.paymentId);
+    this.studentsService.getPaymenDetails(this.paymentId).subscribe(
+      {
+        next:value => {
+          let blob : Blob = new Blob( [value], { type: 'application/pdf' });
+          this.pdfFileUrl = URL.createObjectURL(blob);
+        },
+        error:err => {
+          console.log(err);
+        }
+      }
+    );
   }
 
 
+  afterLoadComplete($event:any) {
 
+  }
 }
